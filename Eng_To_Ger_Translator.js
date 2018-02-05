1981902ed2bc8dfdf47a6c60c3117eb68c0b6ed8
Justin <12138justin@gmail.com>
2018-02-05 02:37:07 +0000

    2nd

--- Eng_To_Ger_Translator.js
+++ Eng_To_Ger_Translator.js
@@ -1,15 +1,21 @@
+/*  Justin Chan Yung Shing 05/02/2018 
+    For opearlo Intern Interview 
+    A simple Eng - Ger translation Alexa skills kit 
+    API used: https://cloud.google.com/translate/docs/ -- You need an API key for this application to work
+*/
+
 'use strict';
-var http = require('http');
+const http = require('https');
+var api = "<<ENTER YOUR API KEY HERE>>"; 
 
 exports.handler = function(event,context) {
 
-    try {
     var request = event.request;
   
     /* 3 types of requests
-    i)   LaunchRequest       Ex: "Open greeter"
-    ii)  IntentRequest       Ex: "Say hello to John" or "ask greeter to say hello to John"
-    iii) SessionEndedRequest Ex: "exit" or error or timeout */
+    i)   LaunchRequest       
+    ii)  IntentRequest       
+    iii) SessionEndedRequest  */
     
     if (request.type === "LaunchRequest") {
         let options = {};
@@ -20,52 +26,28 @@ exports.handler = function(event,context) {
         
     else if (request.type === "IntentRequest" ) {
         let options = {};
-       
-
+        
         if (request.intent.name === "TranslateIntent") {
-        let sentence = request.intent.slots.sentence.value;
-
-        var url = "https://translation.googleapis.com/language/translate/v2?key=AIzaSyDOceu-kUF26jegEkL9EzwkyaBIFofj2MY&target=de" + "&q=" + sentence;
-        var translation = "";
-            
-        http.get(url, function(res){
-        var body = "";
         
-        res.on('data', function(chunk){
-            body += chunk;
-            
+        let sentence = request.intent.slots.sentence.value; 
+        getTranslation(sentence, function(translatedSentence,err){
+            if (err) {
+                context.fail(err);
+            } else {
+                options.cardTitle = "Translation of:" + sentence;
+                options.cardContent = translatedSentence;
+                options.speechText = translatedSentence + ". Sorry that my pronounciation is not very good yet. But I have sent the translation to your alexa app. Is there anything that you need help with?";
+                options.repromptText = "is there anything else that I can do?"
+                options.endSession = false;
+                context.succeed(buildResponse(options));
+            }
         });
-        
-        res.on('end', function(){
-            var tempJson = JSON.parse(body);
-            translation = tempJson.data.translations[0].translatedText;
-        });
-    
-        });
-      
-        options.cardTitle = "Translation of:" + ;
-        options.cardContent = translation;
-        options.speechText = "The translation has been sent to your alexa app on your phone";
-        options.endSession = true;
-        context.succeed(buildResponse(options));
-        }
-        
-        else {
-            throw"unknown intent";
-        }
-        
-    }
+    }}
     else if (request.type === "SessionEndedRequest"){
         
     }
-    else {
-        throw"Unknown intent type";
-    }
 
-}catch (e) {
-    context.fail("Exception:"+e);
-}
-}
+} 
 
 function buildResponse(options) {
     
@@ -78,9 +60,16 @@ var response = {
     },
     "shouldEndSession": options.endSession}
 };
+    if(options.repromptText) {
+        response.response.reprompt = {
+            outputSpeech: {
+            type:"PlainText",
+            text: options.repromptText}
+            
+        };} 
     
     if(options.cardTitle) {
-        response.reponse.card = { 
+        response.response.card = { 
         type: "Simple",
         title: options.cardTitle ,
         content: options.cardContent
@@ -88,4 +77,29 @@ var response = {
     }
     
     return response;
+}
+
+function getTranslation(sentence, callback) {
+    var url = "https://translation.googleapis.com/language/translate/v2?key=" + api +"&target=de" + "&q=" + sentence;
+    var req = http.get(url, function(res){
+    var body = "";
+    
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+    
+    res.on('end', function(){
+        //console.log(body);
+        body = body.replace(/\\/g,'');
+        var content = JSON.parse(body);
+        //console.log(content);
+        callback(content.data.translations[0].translatedText);
+    }); 
+    
+    });
+    
+    req.on('error', function(err){
+    callback('', err);
+    });
+    
 }
